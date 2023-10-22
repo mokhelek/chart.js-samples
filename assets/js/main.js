8a9ebf9a3737ef038f6f263480dd81e1b41b06c4
mokhelek <mkatleho191@gmail.com>
2023-10-22 14:26:56 +0200

    add dark theme + UI enhancements

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,25 +3,27 @@
 //     console.log(data)
 // });
 
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById("myChart");
 
 new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
+    type: "bar",
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+            {
+                label: "# of Votes",
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
 });
 
 const data = {
@@ -65,76 +67,82 @@ const initChart = new Chart(populationStats, config);
 
 // Sample data for the pie chart
 const pieData = {
-    labels: ['Housing', 'Transportation', 'Food', 'Utilities', 'Entertainment', 'Savings', 'Other'],
+    labels: ["Housing", "Transportation", "Food", "Utilities", "Entertainment", "Savings", "Other"],
     datasets: [
-      {
-        data: [1200, 300, 400, 200, 150, 500, 250], // Sample quantities for each fruit
-        backgroundColor: ['red', 'yellow', 'orange', 'purple', 'pink','lightblue','green'], // Colors for each slice
-      },
+        {
+            data: [1200, 300, 400, 200, 150, 500, 250], // Sample quantities for each fruit
+            backgroundColor: ["red", "yellow", "orange", "purple", "pink", "lightblue", "green"], // Colors for each slice
+        },
     ],
-  };
-  
-  const pieConfig = {
-    type: 'pie',
+};
+
+const pieConfig = {
+    type: "pie",
     data: pieData,
     options: {
-      plugins: {
-        legend: {
-          position: 'left', // Adjust legend position
+        plugins: {
+            legend: {
+                position: "left", // Adjust legend position
+            },
         },
-      },
     },
-  };
-  
-  // Assuming you have a canvas element with an id "fruit-chart" in your HTML.
-  const pieElem = document.getElementById('pie-chart').getContext('2d');
-  const pieChart = new Chart(pieElem, pieConfig);
-  
+};
+
+// Assuming you have a canvas element with an id "fruit-chart" in your HTML.
+const pieElem = document.getElementById("pie-chart").getContext("2d");
+const pieChart = new Chart(pieElem, pieConfig);
 
 // ********************** Line Graph ***********************
 
-
 // Sample data for the line graph for a whole year
 const priceData = {
-    labels: [
-      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-    ],
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [
-      {
-        label: 'Stock Price (USD)',
-        data: [
-          50, 25, 30, 32, 58, 46, 70, 68, 43, 75, 65, 67 // Sample stock price over 12 months
-        ],
-        borderColor: 'purple', // Line color
-        fill: true,
-      },
+        {
+            label: "Stock Price (USD)",
+            data: [50, 25, 30, 32, 58, 46, 70, 68, 43, 75, 65, 67],
+            borderColor: "#e595fd",
+            fill: true,
+            title: {
+                display: true,
+                text: "Yearly Stock Prices",
+                color: "red",
+            },
+        },
     ],
-  };
-  
-  const priceConfig = {
-    type: 'line',
+};
+
+const priceConfig = {
+    type: "line",
     data: priceData,
     options: {
-      scales: {
-        x: {
-          beginAtZero: true,
-          title: {
-            display: true,
-            text: 'Months',
-          },
+        scales: {
+            x: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: "Months",
+                    color: "pink",
+                },
+                ticks: {
+                    color: "#c7c7c7", // Change the color of x-axis tick labels
+                },
+            },
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: "Stock Price (USD)",
+                    color: "pink",
+                },
+                ticks: {
+                    color: "#c7c7c7", // Change the color of x-axis tick labels
+                },
+            },
         },
-        y: {
-          beginAtZero: true,
-          title: {
-            display: true,
-            text: 'Stock Price (USD)',
-          },
-        },
-      },
     },
-  };
-  
-  // Assuming you have a canvas element with an id "stock-price-chart" in your HTML.
-  const priceElem = document.getElementById('price-chart').getContext('2d');
-  const priceChart = new Chart(priceElem, priceConfig);
-  
+};
+
+// Assuming you have a canvas element with an id "stock-price-chart" in your HTML.
+const priceElem = document.getElementById("price-chart").getContext("2d");
+const priceChart = new Chart(priceElem, priceConfig);
